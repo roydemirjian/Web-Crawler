f9b8c7450d64bc1808309ea8861ccfdb1be4868b
roydemirjian <roykebab@gmail.com>
2019-02-14 18:20:00 -0500

    Added counters for pages crawled

--- test.py
+++ test.py
@@ -30,18 +30,26 @@ title = clean_title(title)
 #Find links and append to list
 title = soup.find_all('a')
 urls=[]
+numberoflinks = 0
 for links in title:
         urls.append(links.get('href'))
+        numberoflinks += 1
 
 #Open file 
 f = open('crawled_urls.txt',"w")
 
+numberofterms = 0
 #Iterate through list for matching results 
 for items in urls:
     if '/wiki/' in str(items):
         if term in str(items):
-            print("https://en.wikipedia.org" + items)
+            numberofterms += 1
+            #print("https://en.wikipedia.org" + items)
             f.write("https://en.wikipedia.org" + items)
             f.write("\n")
 
 f.close()
+
+print("NUMBER OF LINKS FOUND: " + str(numberoflinks))
+
+print("NUMBER OF RELEVANT LINKS FOUND (USING TERMS): " + str(numberofterms))
