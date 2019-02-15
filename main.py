0ac2f69e567482310bdbbda5466427dcfe38d69c
roydemirjian <roykebab@gmail.com>
2019-02-15 00:50:03 -0500

    Organized

--- main.py
+++ main.py
@@ -1,79 +1,55 @@
-import re
+import re 
 import ssl
 import os
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-#Set up SSL environment
-try:
-    _create_unverified_https_context = ssl._create_default_https_context
-except AttributeError:
-    pass
-else: 
-    ssl._create_default_https_context = _create_unverified_https_context
+#Starting Page
+seed = "https://en.wikipedia.org/wiki/NASA"
 
-#Get page content
-def get_page_content(url):
-    try:   
-        html_response_text = urlopen(url).read()
-        page_content = html_response_text.decode('utf-8')
-        return page_content
-    except Exception as e:
-        return None
+#Search term
+term = 'space'
 
-soup = BeautifulSoup(page_content,'html.parser')
-page_text = soup.get_text()
+#Get html
+htmlResponse = urlopen(seed).read()
+pageContent = htmlResponse.decode('utf-8')
+
+#Parse
+soup = BeautifulSoup(pageContent, 'html.parser')
 title = soup.title.string
-title = clean_title(title)
 
-#Clean the title of a page
+#Clean
 def clean_title(title):
     invalid_characters = ['<','>',':','"','/','\\','|','?','*']
     for c in invalid_characters:
         title = title.replace(c,'')
     return title
 
-#Check if term is included in page text
-if re.search(term, page_text, re.I):
+title = clean_title(title)
 
-#Extract inner urls from page content
-def get_urls(soup):
-    links = soup.find_all('a') 
-    urls=[] 
-    for link in links:
-        urls.append(link.get('href'))
-    return urls
+#Find links and append to list
+title = soup.find_all('a')
+urls=[]
+numberoflinks = 0
+for links in title:
+        urls.append(links.get('href'))
+        numberoflinks += 1
 
-#Check if url is valid
-def is_url_valid(url):
-    if url is None:
-        return False
-    if re.search('#', url):
-        return False
-    match = re.search('^/wiki/', url)
-    if match:
-        return True
-    else:
-        return False
+#Open file 
+f = open('crawled_urls.txt',"w")
 
-#Reformat relative in full url
-def reformat_url(url):
-    match = re.search('^/wiki/',url)
-    if match:
-        return "https://en.wikipedia.org"+url
-    else:
-        return url
+numberofterms = 0
+#Iterate through list for matching results 
+for items in urls:
+    if '/wiki/' in str(items):
+        if term in str(items):
+            numberofterms += 1
+            #print("https://en.wikipedia.org" + items)
+            f.write("https://en.wikipedia.org" + items)
+            f.write("\n")
 
-#Save page
-def save(text,path)
-    f = open(path,'w', encoding = 'utf-8', errors = 'ignore')
-    f.write(text)
-    f.close()
-
-#Save crawled urls
-f = open("crawled_urls.txt","w")
-i = 1
-for url in crawled_urls:
-    f.write(str(i) + ': ' + url + '\n')
-    i += 1
 f.close()
+
+print("NUMBER OF LINKS FOUND: " + str(numberoflinks))
+
+print("NUMBER OF RELEVANT LINKS FOUND (USING TERMS): " + str(numberofterms))
